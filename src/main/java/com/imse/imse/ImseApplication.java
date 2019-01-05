674ea26f260faa014b71e361b901ac587029f783
Aleksandar Milinkovic <a01646014@unet.univie.ac.at>
2019-01-05 16:13:19 +0100

    Initial Commit

---
+++ src/main/java/com/imse/imse/ImseApplication.java
@@ -0,0 +1,14 @@
+package com.imse.imse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImseApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/imse/imse/ImseApplicationTests.java
@@ -0,0 +1,17 @@
+package com.imse.imse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
